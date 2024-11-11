7585922823ccabd276a6647ed6c574f6074b2c9f
Taylor Radtke <taylorradtke@gmail.com>
2024-11-11 17:17:06 -0600

    Update Nav.js

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -10,48 +10,46 @@ import {
 import ResumePDF from "../../assets/resume/taylor-radtke--resume.pdf";
 import { IconResume } from "../Icons";
 
-const Nav = ({ sections }) => {
-  return (
-    <StyledNav>
-      <Scrollspy
-        currentClassName="is-active"
-        items={sections.map(({ id }) => id)}
-        offset={-55}
-      >
-        {sections.map(item => (
-          <StyledNavItem key={item.id}>
-            <StyledNavLink
-              onClick={e => {
-                // Handles Smooth Scroll
-                e.preventDefault();
-                const element = document.getElementById(item.id);
-                if (typeof window !== "undefined") {
-                  const top = element
-                    ? element.getBoundingClientRect().top + window.pageYOffset
-                    : 0;
-                  window.scroll({
-                    top,
-                    left: 0,
-                    behavior: "smooth",
-                  });
-                }
-              }}
-              type="button"
-            >
-              {item.name}
-            </StyledNavLink>
-          </StyledNavItem>
-        ))}
-        <StyledNavItem>
-          <StyledDownloadButton download href={ResumePDF}>
-            <IconResume />
-            Download CV
-          </StyledDownloadButton>
+const Nav = ({ sections }) => (
+  <StyledNav>
+    <Scrollspy
+      currentClassName="is-active"
+      items={sections.map(({ id }) => id)}
+      offset={-55}
+    >
+      {sections.map(({ id, name }) => (
+        <StyledNavItem key={id}>
+          <StyledNavLink
+            onClick={e => {
+              // Handles Smooth Scroll
+              e.preventDefault();
+              const element = document.getElementById(id);
+              if (typeof window !== "undefined") {
+                const top = element
+                  ? element.getBoundingClientRect().top + window.pageYOffset
+                  : 0;
+                window.scroll({
+                  top,
+                  left: 0,
+                  behavior: "smooth",
+                });
+              }
+            }}
+            type="button"
+          >
+            {name}
+          </StyledNavLink>
         </StyledNavItem>
-      </Scrollspy>
-    </StyledNav>
-  );
-};
+      ))}
+      <StyledNavItem>
+        <StyledDownloadButton download href={ResumePDF}>
+          <IconResume />
+          Download CV
+        </StyledDownloadButton>
+      </StyledNavItem>
+    </Scrollspy>
+  </StyledNav>
+);
 
 Nav.propTypes = {
   sections: PropTypes.arrayOf(
